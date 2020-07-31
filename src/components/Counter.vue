dd6301350ab47845c246bfaed8ce8e06d08d66cc
Leon Arndt <leon.arndt@xyrality.com>
2020-07-31 11:45:38 +0200

    Implement basic vuex store

---
+++ src/components/Counter.vue
@@ -0,0 +1,37 @@
+Vue.component('counter', {
+props: ['timeLeft'],
+
+<template>
+          <div>
+              {{ count }}
+          </div>
+</template>
+})
+<script>
+    export default {
+        name: "AttackButton",
+        props: {
+            timeLeft: String,
+            minutesLeft: Number
+        },
+        methods: {
+            makeZero() {
+                this.timeLeft = "0";
+            },
+            getTime() {
+                let num = this.minutesLeft;
+                let hours = (num / 60);
+                let rhours = Math.floor(hours);
+                let minutes = (hours - rhours) * 60;
+                let rminutes = Math.round(minutes);
+                return rhours + ":" + rminutes;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    {
+        text-transform: none !important;
+    }
+</style>
